dae8f6dad24792f2635ad5dbee119d4da50fd191
eneyire <omuyag4@gmail.com>
2026-09-26 16:06:27 +0100

    Rounding up getting file content + including lorem ipsum dummy text

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -20,9 +20,9 @@ def get_file_content(working_directory: str, file_path: str) -> str:
         if not os.path.isfile(target_file_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
 
-        with open(target_file_path, "r") as file:
-            content = file.read(MAX_CHARS)
-            if file.read(1):
+        with open(target_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if f.read(1):
                 content += (
                     f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
                 )
