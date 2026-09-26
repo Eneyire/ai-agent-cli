98ca7a0b940142d931b9f94706f1f5de681db139
eneyire <omuyag4@gmail.com>
2026-09-26 16:38:20 +0100

    added test file

--- test_run_python_file.py
+++ test_run_python_file.py
@@ -1,6 +1,5 @@
 from functions.run_python_file import run_python_file
 
-
 if __name__ == "__main__":
     print('run_python_file("calculator", "main.py"):')
     print(run_python_file("calculator", "main.py"))
